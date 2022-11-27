218084bfd2d434bbae8cfb0f63d31c71d4ccd90d
Bechara SEBAALY <becharak.sebaaly@gmail.com>
2022-11-27 18:48:57 +0100

    feat: added dto and validation
    added an interface that represents the model of that api data, also two type of dto, one reduced which will be used for the cards in the frontend (because not all the data is shown on the cards the other one is expanded which will be used for when users request to see all the info about a restaurant.

---
+++ crous-backend-project-2022/src/crous/dto/api-crous.interface.ts
@@ -0,0 +1,15 @@
+export interface Crous{
+    id: string;
+    type: string;
+    zone: string;
+    title: string;
+    shortDesc: string;
+    address: string;
+    phoneNumber: string;
+    email: string;
+    latitude: number;
+    longitude: number
+    info: string;
+    closing: number;
+    photoURL: string;
+}
---
+++ crous-backend-project-2022/src/crous/dto/expanded-crous.dto.ts
@@ -0,0 +1,72 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Crous } from './api-crous.interface';
+
+export class ExpandedCrousDto {
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  type: string;
+
+  @IsString()
+  @IsNotEmpty()
+  zone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  shortDesc: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phoneNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  latitude: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  longitude: number;
+
+  @IsString()
+  @IsNotEmpty()
+  info: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  closing: number;
+
+  @IsString()
+  @IsNotEmpty()
+  photoURL: string;
+
+  constructor(crous: Crous) {
+    this.id = crous.id;
+    this.type = crous.type;
+    this.zone = crous.zone;
+    this.title = crous.title;
+    this.shortDesc = crous.shortDesc;
+    this.address = crous.address;
+    this.phoneNumber = crous.phoneNumber;
+    this.email = crous.email;
+    this.latitude = crous.latitude;
+    this.longitude = crous.longitude;
+    this.info = crous.info;
+    this.closing = crous.closing;
+    this.photoURL = crous.photoURL;
+  }
+}
